82044b23e147bf5f6ede0a9591d2a2360056dbc5
Eric Lee <me@ericchanlee.com>
2025-12-15 11:02:53 -0800

    feat: loading image and text layer

--- src/c/pebble.c
+++ src/c/pebble.c
@@ -1,60 +1,94 @@
 #include <pebble.h>
 
-static Window *s_window;
-static TextLayer *s_text_layer;
+static Window *s_main_window;
 
-static void prv_select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_text_layer, "Select");
+static BitmapLayer *s_background_layer;
+static GBitmap *s_background_bitmap;
+
+static TextLayer *s_time_layer;
+static GFont s_time_font;
+
+// Updates s_time_layer with current time
+static void update_time() {
+  time_t temp = time(NULL);
+  struct tm *tick_time = localtime(&temp);
+
+  static char s_buffer[8];
+  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ? "%H:%M" : "%I:%M",
+           tick_time);
+
+  text_layer_set_text(s_time_layer, s_buffer);
 }
 
-static void prv_up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_text_layer, "Up");
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
 }
 
-static void prv_down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_text_layer, "Down");
-}
-
-static void prv_click_config_provider(void *context) {
-  window_single_click_subscribe(BUTTON_ID_SELECT, prv_select_click_handler);
-  window_single_click_subscribe(BUTTON_ID_UP, prv_up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, prv_down_click_handler);
-}
-
-static void prv_window_load(Window *window) {
+static void main_window_load(Window *window) {
+  // Get information about the Window
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  s_text_layer = text_layer_create(GRect(0, 72, bounds.size.w, 20));
-  text_layer_set_text(s_text_layer, "Press a button");
-  text_layer_set_text_alignment(s_text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(s_text_layer));
+  window_set_background_color(s_main_window, GColorWhite);
+
+  // Create BitmapLayer for background image
+  s_background_bitmap =
+      gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
+  s_background_layer = bitmap_layer_create(bounds);
+
+  // Initialize background image
+  bitmap_layer_set_compositing_mode(s_background_layer, GCompOpSet);
+  bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+  layer_add_child(window_layer, bitmap_layer_get_layer(s_background_layer));
+
+  // Create the TextLayer with specific bounds
+  s_time_layer = text_layer_create(
+      GRect(0, PBL_IF_ROUND_ELSE(58, 115), bounds.size.w, 50));
+
+  // Initialize time text
+  GFont font_bitham = fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD);
+  text_layer_set_background_color(s_time_layer, GColorClear);
+  text_layer_set_text_color(s_time_layer, GColorPictonBlue);
+  text_layer_set_font(s_time_layer, font_bitham);
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+
+  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
 }
 
-static void prv_window_unload(Window *window) {
-  text_layer_destroy(s_text_layer);
+static void main_window_unload(Window *window) {
+  text_layer_destroy(s_time_layer);
+  bitmap_layer_destroy(s_background_layer);
+  gbitmap_destroy(s_background_bitmap);
+  fonts_unload_custom_font(s_time_font);
 }
 
-static void prv_init(void) {
-  s_window = window_create();
-  window_set_click_config_provider(s_window, prv_click_config_provider);
-  window_set_window_handlers(s_window, (WindowHandlers) {
-    .load = prv_window_load,
-    .unload = prv_window_unload,
-  });
-  const bool animated = true;
-  window_stack_push(s_window, animated);
+static void init() {
+  // Create main Window element and assign to pointer
+  s_main_window = window_create();
+
+  // Set handlers to manage the elements inside the Window
+  window_set_window_handlers(
+      s_main_window,
+      (WindowHandlers){.load = main_window_load, .unload = main_window_unload});
+
+  // Show the Window on the watch, with animated=true
+  window_stack_push(s_main_window, true);
+
+  // Update clock every minute
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  update_time();
+
+  // Set s_time_layer to use MochiBop font
+  ResHandle font_mochibop =
+      resource_get_handle(RESOURCE_ID_FONT_MOCHIBOP_DOS_48);
+  s_time_font = fonts_load_custom_font(font_mochibop);
+  text_layer_set_font(s_time_layer, s_time_font);
 }
 
-static void prv_deinit(void) {
-  window_destroy(s_window);
-}
+static void deinit() { window_destroy(s_main_window); }
 
 int main(void) {
-  prv_init();
-
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", s_window);
-
+  init();
   app_event_loop();
-  prv_deinit();
+  deinit();
 }
